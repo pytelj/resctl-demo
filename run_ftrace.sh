0018ac3e4b8898fb4aa5be4ed7fa18d1388285fd
Jan Pytel <jas.pytel@gmail.com>
2026-05-26 15:38:54 +0100

    Adjust the set of functions traced for fair class

--- run_ftrace.sh
+++ run_ftrace.sh
@@ -195,12 +195,12 @@ start_ftrace() {
 
     echo schedule > set_ftrace_filter
 
-    # fair class (CFS)
+    # fair class (CFS/EEVDF)
     echo pick_next_task_fair >> set_ftrace_filter
-    echo put_prev_task_fair >> set_ftrace_filter
-    echo enqueue_task_fair >> set_ftrace_filter
-    echo dequeue_task_fair >> set_ftrace_filter
-    echo task_tick_fair >> set_ftrace_filter
+    echo pick_task_fair >> set_ftrace_filter
+
+    # CFS/EEVDF internals
+    echo put_prev_entity >> set_ftrace_filter
 
     # sched_ext
     echo pick_task_scx >> set_ftrace_filter
@@ -208,11 +208,6 @@ start_ftrace() {
     echo enqueue_task_scx >> set_ftrace_filter
     echo dequeue_task_scx >> set_ftrace_filter
     echo task_tick_scx >> set_ftrace_filter
-
-    # CFS internals
-    echo put_prev_entity >> set_ftrace_filter
-    echo enqueue_entity >> set_ftrace_filter
-    echo dequeue_entity >> set_ftrace_filter
 
     echo function_graph > current_tracer
     echo 0 > options/sleep-time
