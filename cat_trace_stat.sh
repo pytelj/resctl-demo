3a41488761dff47f5cdfcf787cf67ae3c84192a8
Jan Pytel <jas.pytel@gmail.com>
2026-06-09 00:00:20 +0100

    Rmv unused files

--- cat_trace_stat.sh
+++
@@ -1,4 +0,0 @@
-sudo cat /sys/kernel/debug/tracing/trace_stat/function0
-sudo cat /sys/kernel/debug/tracing/trace_stat/function1
-sudo cat /sys/kernel/debug/tracing/trace_stat/function2
-sudo cat /sys/kernel/debug/tracing/trace_stat/function3
